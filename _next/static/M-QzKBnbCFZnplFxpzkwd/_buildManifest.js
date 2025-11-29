8848b4fe30904cbc04190e94a49fc4d8fef66809
Yeski <yeskialex@gmail.com>
2025-11-30 01:24:12 +0900

    Updates

--- _next/static/M-QzKBnbCFZnplFxpzkwd/_buildManifest.js
+++ _next/static/M-QzKBnbCFZnplFxpzkwd/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio_website//_next/:path+",
+        "destination": "/portfolio_website/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
